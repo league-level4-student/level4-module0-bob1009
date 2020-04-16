f25699dc084f9c78691d697cb314ce157535f792
phili <phili@LAPTOP-OI44HTNG>
2020-04-16 15:29:56 -0700

    almost done with wrong way cow

--- src/extras/the_wrong_way_cow/TheWrongWayCow.java
+++ src/extras/the_wrong_way_cow/TheWrongWayCow.java
@@ -42,20 +42,92 @@
 package extras.the_wrong_way_cow;
 
 public class TheWrongWayCow {
+	int[] moo = new int[2];
+	int[] moo2 = new int[2];
+	int[] moo3 = new int[2];
+	int[] moo4 = new int[2];
 
 	public static int[] findWrongWayCow(final char[][] field) {
+
+		return null;
 		// Fill in the code to return the x,y coordinate position of the
 		// head (letter 'c') of the wrong way cow!
-		int[] moo=new int[2];
-		
+	}
+
+	int[] checkWest(final char[][] field) {
+		int rightwaywest = 0;
 		for (int i = 0; i < field.length; i++) {
 			for (int j = 0; j < field[i].length; j++) {
-				if (field[i][j]=='c') {
-					moo[0]=i;
-					moo[1]=j;
+				if (field[i][j] == 'c' && field[i + 1][j] == 'o' && field[i + 2][j] == 'w') {
+					rightwaywest += 1;
+				} else {
+					moo[0] = i;
+					moo[1] = j;
+
 				}
 			}
 		}
 		return moo;
 	}
+
+	int[] checkEast(final char[][] field) {
+		int rightwayEast = 0;
+		int wrongwayEast = 0;
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length; j++) {
+				if (field[i][j] == 'w' && field[i + 1][j] == '0' && field[i + 1][j] == 'c') {
+					rightwayEast += 1;
+				} else {
+					moo2[0] = i;
+					moo2[i] = j;
+				}
+
+			}
+
+		}
+		return moo2;
+	}
+
+	int[] checkNorth(final char[][] field) {
+		int rightwayNorth = 0;
+		int wrongwayNorth = 0;
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length; j++) {
+				if (field[i][j + 1] == 'c' && field[i][j + 1] == 'o' && field[i][j + 1] == 'w') {
+					rightwayNorth += 1;
+				} else {
+					moo3[0]=i;
+					moo3[1]=j;
+				}
+			}
+		}
+		return moo3;
+	}
+
+	int[] checkSouth(final char[][] field) {
+		int rightwaySouth = 0;
+		int wrongwaySouth = 0;
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length; j++) {
+				if (field[i][j + 1] == 'w' && field[i][j + 1] == '0' && field[i][j + 1] == 'c') {
+					rightwaySouth += 1;
+				} else {
+					moo4[0]=i;
+					moo4[0]=j;
+				}
+			}
+		}
+return moo4;
+	}
+//		void check() {
+//		boolean wrongwaywrong=false;
+//		boolean rightwaywrong=false;
+//		if (rightway>wrongway) {
+//			wrongwaywrong=true;
+//		}
+//		else {
+//		rightwaywrong=true; 
+//		}
+//		}
+
 }
